cc2dff59a4d0fdea8d2ce8b984471f9b7cb6d812
Pavel Ryapolov <paulustg@gmail.com>
2022-09-29 12:41:11 +0300

    Some UI changes for FavoritesPage

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
+    <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-700 text-white">
       <h3 className="font-bold">Github Search</h3>
 
       <span>
-        <Link to="/" className="mr-2">
+        <Link to="/" className="mr-2 hover:text-gray-400">
           Home
         </Link>
-        <Link to="/favorites">Favorites</Link>
+        <Link to="/favorites" className="hover:text-gray-400">
+          Favorites
+        </Link>
       </span>
     </nav>
   );
--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,19 +1,42 @@
 import React from 'react';
+import { useActions } from '../hooks/actions';
 import { useAppSelector } from '../hooks/redux';
 
 function FavoritesPage() {
+  const { removeFavorite } = useActions();
   const { favorites } = useAppSelector((state) => state.github);
 
   if (favorites.length === 0) return <p className="text-center">No items.</p>;
+
+  const removeFromFavorite = (evt: React.MouseEvent<HTMLButtonElement>) => {
+    evt.preventDefault();
+    removeFavorite(evt.currentTarget.id);
+  };
 
   return (
     <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
       <ul className="list-none">
         {favorites.map((item) => (
-          <li key={item}>
-            <a href={item} target="_blank" rel="noreferrer">
+          <li
+            key={`li_${item}`}
+            className="flex justify-between items-center border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all"
+          >
+            <a
+              href={`a_${item}`}
+              target="_blank"
+              rel="noreferrer"
+              className="mr-4"
+            >
               {item}
             </a>
+            <button
+              className="py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all"
+              onClick={removeFromFavorite}
+              key={`btn_${item}`}
+              id={item}
+            >
+              Remove
+            </button>
           </li>
         ))}
       </ul>
